95ff54c78e56de0da1b462203b0b149a1e3cfaf6
astromg01 <antoniomauriciorodriguesalves@gmail.com>
2026-08-17 12:31:34 -0400

    Expand PS1 disc image extensions

--- app/src/main/java/com/omnicore/emulator/model/ConsoleSystem.kt
+++ app/src/main/java/com/omnicore/emulator/model/ConsoleSystem.kt
@@ -7,7 +7,12 @@ enum class ConsoleSystem(
     val generation: String
 ) {
     NINTENDO_64("Nintendo 64", "N64", setOf("z64", "n64", "v64"), "1996"),
-    PLAYSTATION_1("PlayStation", "PS1", setOf("cue", "bin", "chd", "ccd"), "1994"),
+    PLAYSTATION_1(
+        "PlayStation",
+        "PS1",
+        setOf("cue", "bin", "chd", "ccd", "iso", "pbp", "img", "mdf", "cbn", "exe"),
+        "1994"
+    ),
     PLAYSTATION_2("PlayStation 2", "PS2", setOf("iso", "chd"), "2000"),
     PSP("PlayStation Portable", "PSP", setOf("iso", "cso", "pbp"), "2004"),
     WII("Nintendo Wii", "Wii", setOf("rvz", "wbfs", "iso", "gcz"), "2006"),
