9344073e7279e06f15a4fd1714e5486854eaa039
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-18 19:42:38 -0400

    Optimize pinned Play backend fetch

--- tools/fetch_ps2_play_backend.sh
+++ tools/fetch_ps2_play_backend.sh
@@ -6,15 +6,16 @@ DEST="${1:-build/third_party/play}"
 REPO="https://github.com/jpd002/Play-.git"
 
 rm -rf "$DEST"
-mkdir -p "$(dirname "$DEST")"
-git clone --quiet "$REPO" "$DEST"
-git -C "$DEST" checkout --quiet "$PIN"
-git -C "$DEST" submodule update --init --recursive --jobs 2
+mkdir -p "$DEST"
+git -C "$DEST" init -q
+git -C "$DEST" remote add origin "$REPO"
+git -C "$DEST" fetch --quiet --depth 1 origin "$PIN"
+git -C "$DEST" checkout --quiet --detach FETCH_HEAD
+git -C "$DEST" submodule update --init --recursive --depth 1 --jobs 2
 
 test "$(git -C "$DEST" rev-parse HEAD)" = "$PIN"
 test -f "$DEST/License.txt"
 test -f "$DEST/Source/ui_android/CMakeLists.txt"
 test -f "$DEST/Source/CMakeLists.txt"
-
 git -C "$DEST" diff --quiet
 printf 'Play! backend fetched at %s\n' "$PIN"
