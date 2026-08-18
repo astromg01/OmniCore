865e275d089ffd6c46157aafc2c7ca01ba67259f
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-17 22:16:55 -0400

    Bump OmniCore N64 Alpha 5 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.omnicore.emulator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.10.3"
+        versionCode = 20
+        versionName = "0.10.4"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
