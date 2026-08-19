2b349b80e34e931381e9aa58e6b42fc6407ebfe8
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-18 23:40:18 -0400

    Define Play PS2 controller ABI constants

---
+++ app/src/main/java/com/virtualapplications/play/InputManagerConstants.java
@@ -0,0 +1,27 @@
+package com.virtualapplications.play;
+
+/** Numeric ABI contract used by Play!'s generic DualShock 2 pad handler. */
+public final class InputManagerConstants {
+    private InputManagerConstants() {}
+
+    public static final int ANALOG_LEFT_X = 0;
+    public static final int ANALOG_LEFT_Y = 1;
+    public static final int ANALOG_RIGHT_X = 2;
+    public static final int ANALOG_RIGHT_Y = 3;
+    public static final int BUTTON_UP = 4;
+    public static final int BUTTON_DOWN = 5;
+    public static final int BUTTON_LEFT = 6;
+    public static final int BUTTON_RIGHT = 7;
+    public static final int BUTTON_SELECT = 8;
+    public static final int BUTTON_START = 9;
+    public static final int BUTTON_SQUARE = 10;
+    public static final int BUTTON_TRIANGLE = 11;
+    public static final int BUTTON_CIRCLE = 12;
+    public static final int BUTTON_CROSS = 13;
+    public static final int BUTTON_L1 = 14;
+    public static final int BUTTON_L2 = 15;
+    public static final int BUTTON_L3 = 16;
+    public static final int BUTTON_R1 = 17;
+    public static final int BUTTON_R2 = 18;
+    public static final int BUTTON_R3 = 19;
+}
