fd4a653502859db87a35c50990fb8589cc952418
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-18 23:52:35 -0400

    Bump OmniCore to 0.11.1 PS2 Alpha 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.omnicore.emulator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 40
-        versionName = "0.11.0"
+        versionCode = 41
+        versionName = "0.11.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
